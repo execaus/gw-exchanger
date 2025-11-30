7ae8060a3e832b515a8c18fcf16f0889dcd120c1
execaus <execaus@gmail.com>
2025-11-30 16:42:01 +0500

    Добавляет слой обработки запросов

---
+++ internal/handler/handler.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"context"
+	gw_grpc "gw-exchanger/internal/pb/exchange"
+	"gw-exchanger/internal/service"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Handler struct {
+	s *service.Service
+	gw_grpc.UnimplementedExchangeServiceServer
+}
+
+func (h *Handler) GetExchangeRates(ctx context.Context, _ *emptypb.Empty, _ ...grpc.CallOption) (*gw_grpc.ExchangeRatesResponse, error) {
+	m, err := h.s.GetExchangeRates(ctx)
+	if err != nil {
+		zap.L().Error(err.Error())
+		return nil, err
+	}
+
+	return &gw_grpc.ExchangeRatesResponse{
+		Rates: m,
+	}, nil
+}
+
+func (h *Handler) GetExchangeRateForCurrency(ctx context.Context, in *gw_grpc.CurrencyRequest, _ ...grpc.CallOption) (*gw_grpc.ExchangeRateResponse, error) {
+	rate, err := h.s.GetExchangeRateForCurrency(ctx, in.FromCurrency, in.ToCurrency)
+	if err != nil {
+		zap.L().Error(err.Error())
+		return nil, err
+	}
+
+	return &gw_grpc.ExchangeRateResponse{
+		FromCurrency: in.FromCurrency,
+		ToCurrency:   in.ToCurrency,
+		Rate:         rate,
+	}, nil
+}
+
+func NewHandler(srv *service.Service) *Handler {
+	return &Handler{
+		s: srv,
+	}
+}
